adf8ee84c3739cdb99e466ccbd980bed5f4fcecb
Ezequiel H Morales <skyel.programming@gmail.com>
2023-12-09 14:46:51 -0300

    dejé el settings como lo encontré(?

--- Proyecto_Final/Proyecto_Final/settings.py
+++ Proyecto_Final/Proyecto_Final/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog', #Cambiar según nombre de la base de datos local
         'USER': 'root', #Cambiar según usuario de sql local
-        'PASSWORD': 'lw-Rib4L-er1Orn#', #Cambiar según contraseña de sql local
+        'PASSWORD': '', #Cambiar según contraseña de sql local
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '',
     }
 }
 
