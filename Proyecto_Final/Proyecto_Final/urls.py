70b03258607c3648006c3cfdba8f00f7e49f4a77
mangadetransas <mauroblanco544@gmail.com>
2023-12-09 09:42:13 -0300

    nuevos archivos

--- Proyecto_Final/Proyecto_Final/urls.py
+++ Proyecto_Final/Proyecto_Final/urls.py
@@ -21,7 +21,6 @@ from . import views #importamos los views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', views.index_view, name='index'), # enlazamos el index con el views
-    path('publicaciones/', views.publicaciones_view, name='publicaciones' )
+    path('publicaciones', views.publicaciones_view, name='publicaciones' )
 ] 
 
-akjshdfkjahsjaksjdhkjashdkjaskjdhkasd
