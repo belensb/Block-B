7013e0ed83b2c607af54d8d76ddf837d1b7f4bb6
BelenG <90286485+SBelenB@users.noreply.github.com>
2023-12-13 18:31:10 -0300

    Se definió el modelo y una view publicaciones para renderizarlas en el template

--- publicaciones/admin.py
+++ publicaciones/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Publicacion
 # Register your models here.
+admin.site.register(Publicacion) #Registramos el modelo Publicacion para que esté disponible desde el panel de admin
--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import Publicacion
 
 # Crea tus vistas aqui.
 
-def publicaciones_view(request):                  #definimos el publicaciones_view y le hacemos un request para que solicite el archivo
- return render(request, 'publicaciones.html', {}) #retornamos la solicitud y le pedimos que haga el render
+def publicaciones_view(request):  #definimos el publicaciones_view y le hacemos un request para que solicite el archivo
+ #creamos una variable a la que se le asigan un diccionario que se utilizará para traer los datos de la db
+ ctx = {
+  'publicaciones' : Publicacion.objects.all().order_by('fecha') #hacemos la solicitud para que la db nos devuelva todo de la tabla publicaciones
+ }
+
+ return render(request, 'publicaciones.html', ctx) #retornamos la solicitud y le pedimos que haga el render
 
