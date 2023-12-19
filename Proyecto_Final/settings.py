7874cc0ebfe5978b2ebaf1c67163d23217aadc28
BelenG <90286485+SBelenB@users.noreply.github.com>
2023-12-19 01:25:56 -0300

    Creé el nuevo modelo para usuarios y lo definí en settings.py

--- Proyecto_Final/settings.py
+++ Proyecto_Final/settings.py
@@ -15,9 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Definimos el modelo de usuarios
+AUTH_USER_MODEL = 'usuarios.Usuario'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3yqea^sc+8$7xor$q6n2@4*$3g54^(@ys9y@9!1b$aydru4=1b'
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Usuarios(AbstractUser):
+    telefono=models.CharField(max_lenght=50, null=True, blank=True)
+    domicilio=models.CharField(max_lenght=50, null=True, blank=True)
+    es_colaborador = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
