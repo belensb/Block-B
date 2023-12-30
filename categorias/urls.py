669a0c68268f94f1bd29e94c061ed29c6171a07a
BelenG <90286485+SBelenB@users.noreply.github.com>
2023-12-30 20:52:07 -0300

    Correccion para que admins y colaboradores puedan borrar y editar comentarios, se borran urls y views sin uso

--- categorias/urls.py
+++ categorias/urls.py
@@ -3,12 +3,6 @@ from . import views #importamos los views
 
 urlpatterns = [
     path ('ver-categorias/', views.categorias_view, name='ver-categorias'),
-    path('intro-romance/', views.romance_view, name='romance'),
-    path('intro-terror/', views.terror_view, name='terror'),
-    path('intro-ficcion/', views.ficcion_view, name='ficcion'),
-    path('intro-ciencia/', views.ciencia_view, name='ciencia'),
-    path('intro-historia/', views.historia_view, name='historia'),
-    path('intro-guerra/', views.guerra_view, name='guerra'),
     path('<str:nombre_categoria>/', views.publicaciones_por_categoria, name='publicaciones_por_categoria'),
 ]
 
--- categorias/views.py
+++ categorias/views.py
@@ -5,24 +5,6 @@ from publicaciones.models import Publicacion
 def categorias_view(request): #definimos el categorias_view y le hacemos un request para que solicite el archivo
  return render(request, 'categorias/categorias.html', {}) #retornamos la solicitud y le pedimos que haga el render
 
-def romance_view(request): #definimos el romance_view y le hacemos un request para que solicite el archivo
- return render(request, 'categorias/romance.html', {}) #retornamos la solicitud y le pedimos que haga el render
-
-def terror_view(request): #definimos el terror_view y le hacemos un request para que solicite el archivo
- return render(request, 'categorias/terror.html', {}) #retornamos la solicitud y le pedimos que haga el render
-
-def ficcion_view(request): #definimos el ficcion_view y le hacemos un request para que solicite el archivo
- return render(request, 'categorias/ficcion.html', {}) #retornamos la solicitud y le pedimos que haga el render
-
-def ciencia_view(request): #definimos el ciencia_view y le hacemos un request para que solicite el archivo
- return render(request, 'categorias/ciencia.html', {}) #retornamos la solicitud y le pedimos que haga el render
-
-def historia_view(request): #definimos el historia_view y le hacemos un request para que solicite el archivo
- return render(request, 'categorias/historia.html', {}) #retornamos la solicitud y le pedimos que haga el render
-
-def guerra_view(request): #definimos el guerra_view y le hacemos un request para que solicite el archivo
- return render(request, 'categorias/guerra.html', {}) #retornamos la solicitud y le pedimos que haga el render
-
 def publicaciones_por_categoria(request, nombre_categoria):
     # Filtrar las publicaciones por la categoría
     publicaciones = Publicacion.objects.filter(categoria__nombre=nombre_categoria)
